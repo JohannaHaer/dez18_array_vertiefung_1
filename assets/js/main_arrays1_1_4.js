b4e9feedc96a86d68c2cdad0a53b7a700028497e
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 14:01:22 +0100

    1.4 fertig

---
+++ assets/js/main_arrays1_1_4.js
@@ -0,0 +1,12 @@
+let person = ["Johanna", "w", "30"]
+let pushPerson = person.push("blond", "blau")
+console.log(person);
+
+let friends = ["Robin", "Victor", "Daniel"]
+console.log(friends);
+let pushFriends = friends.push("Sarah", "Kim")
+console.log(friends);
+
+let favoriteFood = ["Klöße", "Rotkohl", "Ente"]
+let pushFavFood = favoriteFood.push("Soße", "Dessert")
+console.log(favoriteFood);
