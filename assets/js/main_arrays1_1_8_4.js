8b96862b6f96bc18dab23862c469fad23d7a616f
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 16:32:45 +0100

    1.8_3 und 1.8_4 fertig

--- assets/js/main_arrays1_1_8_4.js
+++ assets/js/main_arrays1_1_8_4.js
@@ -3,3 +3,13 @@ console.log(zahlen);
 
 let zahlenKleiner20 = zahlen.filter(num => num <= 20)
 console.log(zahlenKleiner20);
+
+
+
+let multiplizieren = (zahl) => {
+    return zahl *2
+    
+}
+let zahlenMal2 = zahlenKleiner20.map(multiplizieren)
+console.log(zahlenMal2);
+multiplizieren()
