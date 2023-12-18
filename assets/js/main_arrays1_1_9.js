ba826f3168b926b9eeb2382183679788e9aa3885
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 14:28:42 +0100

    1.9 fertig

--- assets/js/main_arrays1_1_9.js
+++ assets/js/main_arrays1_1_9.js
@@ -2,3 +2,11 @@ let numbers = [23, 54, 75]
 console.log(numbers);
 let pushNumbers = numbers.push(2, 7, 19, 3, 1)
 console.log(numbers);
+let unshiftNumbers = numbers.unshift(52, 13, 47, 4, 9)
+console.log(numbers);
+numbers.pop()
+numbers.pop()
+console.log(numbers);
+numbers.shift()
+numbers.shift()
+console.log(numbers);
