dd189e3df06cd629127e4f085abdc0911e5399f7
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 14:51:35 +0100

    1.8_2 fertig

--- assets/js/main_arrays1_1_8_2.js
+++ assets/js/main_arrays1_1_8_2.js
@@ -1,1 +1,14 @@
+const woerter = ["Banane", "Kaktus", "Flausch", "Ente", "Apfel", "Auto", "Giraffe", "Schmetterling", "Krokodil", "Lampe"];
+console.log(woerter);
+
+let func = () => {
+    let result = woerter.filter(wort => wort.length <= 6)
+    console.log(result);
+}
+
+func()
+
+
+let result = woerter.filter(wort => wort.length <= 6)
+console.log(result);
 
