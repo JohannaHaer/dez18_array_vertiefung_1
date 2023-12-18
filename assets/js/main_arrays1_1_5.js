e7e59f6f6482a6d084ca8f207e4451bf707a6f94
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 14:11:43 +0100

    1.5 fertig

--- assets/js/main_arrays1_1_5.js
+++ assets/js/main_arrays1_1_5.js
@@ -1,11 +1,20 @@
 let person = ["Johanna", "w", "30"]
 let pushPerson = person.push("blond", "blau")
 console.log(person);
+let popPerson = person.pop()
+console.log(popPerson);
+console.log(person);
 
 let friends = ["Robin", "Victor", "Daniel"]
 let pushFriends = friends.push("Sarah", "Kim")
 console.log(friends);
+let popFriends = friends.pop()
+console.log(popFriends);
+console.log(friends);
 
 let favoriteFood = ["Klöße", "Rotkohl", "Ente"]
 let pushFavFood = favoriteFood.push("Soße", "Dessert")
+console.log(favoriteFood);
+let popFavFood = favoriteFood.pop()
+console.log(popFavFood);
 console.log(favoriteFood);
