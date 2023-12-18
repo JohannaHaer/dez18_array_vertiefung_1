8fe9ed80b85c6dea6bfa64ba43f1edf94a352441
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 15:10:54 +0100

    1.8_3 und 1.8_4 fertig

--- assets/js/main_arrays1_1_8_3.js
+++ assets/js/main_arrays1_1_8_3.js
@@ -1,1 +1,5 @@
+const heroArr = ["Superman", "Batman", undefined, , "Wonder Woman", "Spider-Man", "Black Widow", "Iron Man", "Thor", "Catwoman", , null];
+console.log(heroArr);
 
+let myHeros = heroArr.filter(element => element !== null && element !== undefined)
+console.log(myHeros);
--- assets/js/main_arrays1_1_8_4.js
+++ assets/js/main_arrays1_1_8_4.js
@@ -1,1 +1,5 @@
+const zahlen = [12, 25, 7, 18, 30, 5];
+console.log(zahlen);
 
+let zahlenKleiner20 = zahlen.filter(num => num <= 20)
+console.log(zahlenKleiner20);
