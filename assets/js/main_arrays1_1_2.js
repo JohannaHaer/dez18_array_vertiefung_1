777d1040c0494731ac878efb2619fcd27bf52802
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-18 13:55:14 +0100

    1.3 fertig

---
+++ assets/js/main_arrays1_1_2.js
@@ -0,0 +1,18 @@
+let person = ["Johanna", "w", "30"]
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+let friends = ["Robin", "Victor", "Daniel"]
+console.log(friends);
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+let favoriteFood = ["Klöße", "Rotkohl", "Ente"]
+console.log(favoriteFood);
+console.log(favoriteFood[0]);
+console.log(favoriteFood[1]);
+console.log(favoriteFood[2]);
+
---
+++ assets/js/main_arrays1_1_3.js
@@ -0,0 +1,8 @@
+let person = ["Johanna", "w", "30"]
+console.log(person.length);
+
+let friends = ["Robin", "Victor", "Daniel"]
+console.log(friends.length);
+
+let favoriteFood = ["Klöße", "Rotkohl", "Ente"]
+console.log(favoriteFood.length);
